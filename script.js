78f8814ab4b4abb7c87d4de231029683ce278a62
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-01-13 18:45:29 +0300

    numrows func

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 //params
 const RangeX = document.getElementById('X'),
   RangeY = document.getElementById('Y'),
+  NumContainerX = document.getElementById('cube_rt'),
+  NumContainerY = document.getElementById('cube_lb'),
   RangeIterrations = document.getElementById('Iterrations'),
   SwapButton = document.getElementById('swapbtn'),
   RandomNumFromMRange = document.getElementById('M_from_range'),
   Container = document.getElementById('cube_rb');
+
+const CubeWidthWithGap =
+  NumContainerX.children[1].getBoundingClientRect().left -
+  NumContainerX.children[0].getBoundingClientRect().left;
+
 let Matrix = [];
 
 //listeners
 addEventListener('DOMContentLoaded', () => {
-  drawMatrix(Matrix);
+  drawMatrix();
 });
 
 [RangeX, RangeY].forEach((e) =>
   e.addEventListener('input', () => {
-    drawMatrix(Matrix);
+    drawMatrix();
   })
 );
 
@@ -26,9 +33,30 @@ SwapButton.addEventListener('click', async () => {
 });
 
 //functions
-function drawMatrix(matrix) {
+function drawMatrix() {
   Matrix = generateMatrix(RangeX.value, RangeY.value);
+  let [numRangeX, numRangeY] = drawRowsNums(Matrix);
+  NumContainerX.replaceChildren(...numRangeX);
+  NumContainerY.replaceChildren(...numRangeY);
   Container.replaceChildren(...Matrix);
+}
+
+function drawRowsNums(matrix) {
+  let sizeX = matrix[0].children.length,
+    sizeY = matrix.length;
+  let numRangeX = [],
+    numRangeY = [];
+  for (let i = 0; i < sizeY; i++) {
+    numRangeY[i] = document.createElement(`div`);
+    numRangeY[i].textContent = i + 1;
+    numRangeY[i].classList.add(`num_cube`);
+  }
+  for (let i = 0; i < sizeX; i++) {
+    numRangeX[i] = document.createElement(`div`);
+    numRangeX[i].textContent = i + 1;
+    numRangeX[i].classList.add(`num_cube`);
+  }
+  return [numRangeX, numRangeY];
 }
 
 //generate matrix
@@ -40,7 +68,7 @@ function generateMatrix(SizeX, SizeY) {
     for (let j = 0; j < SizeX; j++) {
       matrix[i][j] = document.createElement(`div`);
       matrix[i][j].textContent = j;
-      matrix[i][j].style = `transform:translateX(${j * 70}px);`;
+      matrix[i][j].style = `transform:translateX(${j * CubeWidthWithGap}px);`;
       matrix[i][j].classList.add(`cube`, j < SizeX / 2 && 'green');
       matrix[i].appendChild(matrix[i][j]);
     }
@@ -62,7 +90,9 @@ function swapCubes(matrix) {
               matrix[i][j],
             ];
             for (let z = 0; z < SizeX; z++) {
-              matrix[i][z].style = `transform:translateX(${z * 70}px)`;
+              matrix[i][z].style = `
+              transform:translateX(${z * CubeWidthWithGap}px)
+              `;
             }
             clearTimeout(b);
             clearTimeout(q);
